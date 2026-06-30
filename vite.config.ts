51be4f7a7a7a637cb6806fde192206aea9d93c5b
Analista de Sistemas Jr <ASISTEMAS2@colplast.local>
2026-06-30 16:32:34 -0500

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gonzv_Tech_/', // 👈 IMPORTANTE: debe coincidir con tu repo
+base: '/gonzv-tech/',
 })
